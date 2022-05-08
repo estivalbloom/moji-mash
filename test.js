0cb305b8be59f51f91eae8db9eff2b4a83c19082
Justin Niosi <justinniosi@gmail.com>
2022-05-07 23:00:48 -0400

    Removed test files

--- test.js
+++
@@ -1,8 +0,0 @@
-import { init, randomEmoji, list as emojiList } from "./src/makeEmoji.js";
-import * as fs from 'fs/promises';
-
-init().then(() => {
-    const { canvas, list } = randomEmoji(192);
-    console.log(list);
-    fs.writeFile('./test.png', canvas.toBuffer());
-});

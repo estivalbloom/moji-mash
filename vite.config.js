af952c4747310dcba5daa74d833a802c12207e12
estivalbloom <estivalbloom@gmail.com>
2025-03-02 23:45:46 -0500

    Applies gitignore changes part 2

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from "vite"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+	esbuild: {
+		supported: {
+			'top-level-await': true
+		}
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.js'),
+			name: 'MojiMash',
+			fileName: 'moji-mash',
+			formats: ['es'],
+		}
+	}
+})
